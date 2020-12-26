007a3691eba93165c0e70681510af2b713e8c05d
Maneet Lodha <36129353+maneetlodha28@users.noreply.github.com>
2020-12-27 01:10:36 +0530

    inital commit

---
+++ build/precache-manifest.0ccb4c4ba1aabc5bfea1fe879d85f556.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f38275334fd2926125aaa8635717a0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fba8b01c57368591e5bd",
+    "url": "/static/css/2.4bedae8a.chunk.css"
+  },
+  {
+    "revision": "51058b8310196b904a1d",
+    "url": "/static/css/main.bcb5a572.chunk.css"
+  },
+  {
+    "revision": "fba8b01c57368591e5bd",
+    "url": "/static/js/2.664da7e5.chunk.js"
+  },
+  {
+    "revision": "2520f30cb4e102284a8f5fa3a821a3bf",
+    "url": "/static/js/2.664da7e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51058b8310196b904a1d",
+    "url": "/static/js/main.14b3b7ee.chunk.js"
+  },
+  {
+    "revision": "c7cb78d65c7ae0baf90a",
+    "url": "/static/js/runtime-main.c1fcbbc2.js"
+  }
+]);
